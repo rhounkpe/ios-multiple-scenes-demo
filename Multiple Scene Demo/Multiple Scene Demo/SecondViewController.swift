b2a4a0c0cfe9d275221f07a9763b4046009702b3
Rufin Hounkpe <rhounkpe@gmail.com>
2020-07-25 12:49:25 +0200

    Adding a navigation Controller to an existing project

--- Multiple Scene Demo/Multiple Scene Demo/SecondViewController.swift
+++ Multiple Scene Demo/Multiple Scene Demo/SecondViewController.swift
@@ -14,6 +14,7 @@ class SecondViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        self.title = "Second View Controller"
     }
     
 

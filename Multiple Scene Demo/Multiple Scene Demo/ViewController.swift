4e4962c12b278ab2e763f2961b4148b3500b249a
Rufin Hounkpe <rhounkpe@gmail.com>
2020-07-25 12:35:22 +0200

    Segueing to new IOS View Controller

---
+++ Multiple Scene Demo/Multiple Scene Demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Multiple Scene Demo
+//
+//  Created by Rufin Hounkpe on 25/07/2020.
+//  Copyright © 2020 Digit Com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
